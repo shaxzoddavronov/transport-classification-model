06ea53cfb848794a28defa215c34ceacafd4641c
shaxzoddavronov <shaxzoddavronov0@gmail.com>
2023-06-26 11:35:27 +0500

    Files uploading

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 24 22:00:44 2023
+
+@author: OneComputers
+"""
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title('Transport Images Classification Model')
+file=st.file_uploader('Upload image',type=['png','jpeg','gif','svg'])
+if file:
+    st.image(file)
+    img=PILImage.create(file)
+
+    #model
+    model=load_learner('D:\\DL_model\\transports\\venv\\transport_model.pkl')
+    pred,pred_id,prob=model.predict(img)
+    st.success(f"Transport: {pred}")
+    st.info(f"Probability: {prob[pred_id]*100:.2f}")
+
+    #plotting
+    fig=px.bar(x=model.dls.vocab,y=prob*100)
+    st.plotly_chart(fig)
+print(fastai.__version__)    
